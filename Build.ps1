5f68929bd9db592fde991cb787b441f4e162576a
VR <vrnithinkumar@gmail.com>
2017-12-09 18:32:42 +0530

    Adding the build script

---
+++ Build.ps1
@@ -0,0 +1,35 @@
+#Build script for the Project
+param(
+    [switch]$Build,
+    [switch]$Rebuild,
+    [switch]$Sync,
+    [switch]$Clean,
+    [switch]$Test,
+)
+
+$CommandToExecute =""
+
+if($Build)
+{
+
+}
+
+if($ReBuild)
+{
+
+}
+
+if($Sync)
+{
+
+}
+
+if($Clean)
+{
+
+}
+
+if($Test)
+{
+
+}
